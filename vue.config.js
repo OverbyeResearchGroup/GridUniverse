c5e85938f9bfb5fb7091c6c6b9201868d8f81635
Zeyu Mao <mao.mzy@gmail.com>
2021-11-29 22:41:40 -0600

    1. Remove map is the solution to minimize the dist size.

--- vue.config.js
+++ vue.config.js
@@ -5,19 +5,57 @@ const LodashModuleReplacementPlugin = require("lodash-webpack-plugin");
 
 module.exports = {
   // publicPath: '/~zeyumao2/',
+  productionSourceMap: false,
   transpileDependencies: [
     'vue-echarts',
     'resize-detector',
     'vuetify'
   ],
-  chainWebpack: (config) => {
+  chainWebpack: config => {
     config.plugin("loadshReplace").use(new LodashModuleReplacementPlugin());
+    config.module
+      .rule('images')
+      .use('image-webpack-loader')
+      .loader('image-webpack-loader')
+      .options({
+        bypassOnDebug: true
+      })
+      .end()
   },
   configureWebpack: {
     // Configuration applied to all builds
     optimization: {
       splitChunks: {
-        chunks: 'all'
+        // chunks: 'all'
+        cacheGroups: {
+          vendor: {//第三方库抽离
+            chunks: 'all',
+            test: /node_modules/,
+            name: 'vendor',
+            minChunks: 1,//在分割之前，这个代码块最小应该被引用的次数
+            maxInitialRequests: 5,
+            minSize: 0,//大于 0 个字节
+            priority: 100//权重
+          },
+          common: {  //公用模块抽离
+            chunks: 'all',
+            test: /[\\/]src[\\/]js[\\/]/,
+            name: 'common',
+            minChunks: 2, //在分割之前，这个代码块最小应该被引用的次数
+            maxInitialRequests: 5,
+            minSize: 0,//大于 0 个字节
+            priority: 60
+          },
+          styles: { //样式抽离
+            name: 'styles',
+            test: /\.(sa|sc|c)ss$/,
+            chunks: 'all',
+            enforce: true
+          },
+          runtimeChunk: {
+            name: 'manifest'
+          }
+        }
       },
       nodeEnv: 'production',
       minimize: true
